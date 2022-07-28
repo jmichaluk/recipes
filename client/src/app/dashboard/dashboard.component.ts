86ce8851c938eb6c4b82d58e8d119f9ef49a2184
Jordyn Michaluk <michaluk.jordyn@gmail.com>
2022-07-28 15:29:19 -0500

    Add meal type filtering to meal randomization.

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -14,30 +14,50 @@ export class DashboardComponent implements OnInit {
   mealRandomizerForm: FormGroup = new FormGroup({});
   randomizedMeals$: Observable<Meal[]> = new Observable
 
-  // TODO: Add additional filters to randomizations (meal type)
   // TODO: Add shopping list
 
   constructor(
     private mealService: MealService,
     private fb: FormBuilder
-    ) { }
+  ) { }
 
-    get count() { return this.mealRandomizerForm.get('count')!.value; }
+  get count() { return this.mealRandomizerForm.get('count')!.value; }
+  get meatless() { return this.mealRandomizerForm.get('meatless')?.value; }
+  get chicken() { return this.mealRandomizerForm.get('chicken')?.value; }
+  get turkey() { return this.mealRandomizerForm.get('turkey')?.value; }
+  get other() { return this.mealRandomizerForm.get('other')?.value }
    
   ngOnInit(): void {
     this.mealRandomizerForm = this.fb.group({
-      count: []
+      count: [],
+      meatless: [],
+      chicken: [],
+      turkey: [],
+      other: []
     })
   }
 
   randomizeMeals() {
-    const count = this.count;
+    const count : number = this.count;
+    const isMeatless: Boolean = this.meatless;
+    const isChicken: Boolean = this.chicken;
+    const isTurkey: Boolean = this.turkey;
+    const isOther: Boolean = this.other;
 
-    console.log("Randomize Meals" + count);
+    console.log("Randomize Meals" + count + " : " + isMeatless + isChicken + isTurkey + isOther);
 
     this.randomizedMeals$ = this.mealService.getMeals()
       .pipe(map(meals => meals.sort(
         () => 0.5 - Math.random()
+      ).filter(meal => {
+        switch(meal.type?.toLowerCase()){
+          case("meatless"): return isMeatless;
+          case("chicken"): return isChicken;
+          case("turkey"): return isTurkey;
+          case("other"): return isOther
+        } 
+        return true;
+      }
       ).slice(0,count)))
   }
 }
